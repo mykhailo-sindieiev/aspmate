3775be8dec986c05485b26a4e4dc89a0476da133
Mykhailo Sindieiev <9822870+mykhailo-sindieiev@users.noreply.github.com>
2025-07-26 15:17:39 +0200

    add constructor for defectdojo class

--- aspmwarp/defectdojo/defectdojo.py
+++ aspmwarp/defectdojo/defectdojo.py
@@ -1,4 +1,15 @@
 import requests
 
 class DefectDojo:
-    pass
+    def __init__(self, url, key):
+        self.url = url
+        self.key = key
+        self.headers = {'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                        'Authorization': 'Bearer ' + key}
+        self.session = requests.Session()
+        self.session.headers.update(self.headers)
+
+
+    def close(self):
+        self.session.close()
