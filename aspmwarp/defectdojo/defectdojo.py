cb2781372d1b216fec109d631a9a666edbc57a32
Mykhailo Sindieiev <9822870+mykhailo-sindieiev@users.noreply.github.com>
2025-07-26 15:34:14 +0200

    add draft of the create product function

--- aspmwarp/defectdojo/defectdojo.py
+++ aspmwarp/defectdojo/defectdojo.py
@@ -1,4 +1,5 @@
 import requests
+from urllib.parse import urljoin
 
 class DefectDojo:
     def __init__(self, url, key):
@@ -10,6 +11,21 @@ class DefectDojo:
         self.session = requests.Session()
         self.session.headers.update(self.headers)
 
+        # API endpoints
+        self.CREATE_PRODUCT_API = '/'
+        self.DELETE_PRODUCT_API = '/products/'
+
 
     def close(self):
         self.session.close()
+
+    def create_product(self, name: str) -> int:
+        """
+        Create a new product in the DefectDojo
+
+        :param name: name of the product to create
+        :return: status code, answer in json format
+        """
+        create_url = urljoin(self.url, self.CREATE_PRODUCT_API)
+        resp = self.session.post(url=create_url, json={'name': name})
+
